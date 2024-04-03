9ad3f9809188ff61f2527c5561f155ec17985d40
Goohyun3436 <rngus3436@g.kmou.ac.kr>
2024-04-03 10:22:59 +0900

    Add: Geometry 생성

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -33,6 +33,7 @@ const GlobalStyle = createGlobalStyle`
   
   body {
     margin: 0;
+    padding: 0;
     font-family: "Pretendard GOV", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     letter-spacing: 0.5px;
     -webkit-font-smoothing: antialiased;
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,12 +1,22 @@
+import { Canvas } from '@react-three/fiber';
 import styled from 'styled-components';
+import Cube from '../../components/3DEl/Cube';
 
 const Main = () => {
-  return <MainBox>main</MainBox>;
+  return (
+    <MainBox>
+      <Canvas>
+        <Cube />
+      </Canvas>
+    </MainBox>
+  );
 };
 
 const MainBox = styled.div`
   display: flex;
   align-items: center;
+  width: 100%;
+  height: 100vh;
 `;
 
 export default Main;
